462b1bed3b058c4b15cefcf7bc7288a2b66a30c6
Wes Auyeung <wfa207@gmail.com>
2019-01-03 15:49:55 -0500

    * Added custom snippets directory for vim

--- setup_scripts/write_sym_links.sh
+++ setup_scripts/write_sym_links.sh
@@ -6,13 +6,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # Get our project directory (assumes one level up)
 PROJ_DIR=$( dirname $DIR )
 
-# (Assumes our files are located in the ./src dir)
-SRC_FILE_DIR="${PROJ_DIR}/src"
-
 # Source pretty_print helper
 . ${PROJ_DIR}/helper_scripts/pretty_print.sh
 
 pretty_print "Writing sym links"
+
+# Dot Files ============================================================
+
+# (Assumes our files are located in the ./src dir)
+SRC_FILE_DIR="${PROJ_DIR}/src"
 
 # Write powerline-shell config sym link
 # Separate from others; does *not* go directly in home (~) directory
@@ -20,7 +22,7 @@ mkdir -p ~/.config/powerline-shell
 ln -Fhs "${SRC_FILE_DIR}/powerline-config.json" ~/.config/powerline-shell/config.json
 pretty_print "Wrote powerline-config sym link"
 
-# Write sym links that belong in home (~) directory
+# Write dotfile sym links that belong in home (~) directory
 for abs_file_path in "${SRC_FILE_DIR}"/*; do
 
 	# Get base file name (without path)
@@ -34,7 +36,26 @@ for abs_file_path in "${SRC_FILE_DIR}"/*; do
 	fi
 done
 
+pretty_print "Wrote powerline-config sym link"
+
+# UltiSnips ============================================================
+
+SNIPPET_FILE_DIR="${PROJ_DIR}/snippets"
+
+mkdir -p ~/.vim/my_snippets
+pretty_print "Created snippets directory in vim directory"
+
+for abs_file_path in "${SNIPPET_FILE_DIR}"/*; do
+
+	# Get base file name (without path)
+	base_name=$(basename "${abs_file_path}")
+
+	ln -Fhs ${abs_file_path} ~/.vim/my_snippets/${base_name}
+	pretty_print "Wrote ${base_name} sym link"
+
+# Epilogue =============================================================
+
 pretty_print "All sym links written.\n"
 
 # Clean up namespace
-unset file_name base_name DIR PROJ_DIR SRC_FILE_DIR pretty_print
+unset file_name base_name DIR PROJ_DIR SRC_FILE_DIR SNIPPET_FILE_DIR pretty_print
