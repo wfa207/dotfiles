dbbd84e9c2a481ce1130e5941cfb2f3f3a4f3cbb
Wes Auyeung <wes.auyeung@datadoghq.com>
2021-04-14 18:20:50 -0400

    * Remove files rendered obsolete by refactor

--- helper_scripts/pretty_print.sh
+++
@@ -1,6 +0,0 @@
-pretty_print() {
-	[ -z ${PRINT_COLOR} ] && export PRINT_COLOR="\e[1;36;49m"
-	[ -z ${DEFAULT_COLOR} ] && export DEFAULT_COLOR="\e[0m"
-
-	printf "\n${PRINT_COLOR}${1}${DEFAULT_COLOR}\n"
-}
