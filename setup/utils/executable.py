abf950987c617613db5ab6c433a9f24f21bdf6de
Wes Auyeung <wes.auyeung@datadoghq.com>
2021-04-15 18:08:43 -0400

    * Refactor executable utility to use shell utility

--- setup/utils/executable.py
+++ setup/utils/executable.py
@@ -1,16 +1,16 @@
-import subprocess
+from setup.utils.shell import Shell
 
 
 class Executable:
     @classmethod
     def install(cls, executable_name, **options):
         cli_options = cls._get_cli_options(**options)
-        subprocess.call(["brew", "install", executable_name, *cli_options])
+        Shell.execute("brew", "install", executable_name, *cli_options)
 
     @classmethod
     def uninstall(cls, executable_name, **options):
         cli_options = cls._get_cli_options(**options)
-        subprocess.call(["brew", "uninstall", executable_name, *cli_options])
+        Shell.execute("brew", "uninstall", executable_name, *cli_options)
 
     @classmethod
     def _get_cli_options(cls, **options):
